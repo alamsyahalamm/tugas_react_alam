8cd670cfc3d766a55aa15e54e3179bf9a4ecd12f
Firman-001 <pirman475@smk.belajar.id>
2024-05-03 15:13:56 +0700

    syahh

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="box">
+        <h1 className="title">Hi, Saya Alamsyah</h1>
+        <hr></hr>
+        <p className="paragraph">
+          Saya dari Kelas 12 
+        </p>
+      </div>
+    </div>
+  );
+}
+export default App;
